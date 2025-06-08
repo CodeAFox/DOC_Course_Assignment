9c3bd9da0abc89affc98cbfbea565431af17bc25
CodeAFox <illeskrisztinacsilla@gmail.com>
2025-06-08 15:50:09 +0200

    Backend: Initial Commit

---
+++ Backend/viabackend/src/main/java/via_tabloid_backend/viabackend/ViabackendApplication.java
@@ -0,0 +1,13 @@
+package via_tabloid_backend.viabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViabackendApplication.class, args);
+	}
+
+}
---
+++ Backend/viabackend/src/test/java/via_tabloid_backend/viabackend/ViabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package via_tabloid_backend.viabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
